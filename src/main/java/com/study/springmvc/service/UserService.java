74f95c0028ced06d866c7f5ff8b2616518f5766b
mtrl1514 <mtrl1514@gmail.com>
2016-11-17 00:23:37 +0900

    SpringAngularJS

---
+++ src/main/java/com/study/springmvc/service/UserService.java
@@ -0,0 +1,27 @@
+package com.study.springmvc.service;
+
+import java.util.List;
+
+import com.study.springmvc.model.User;
+
+
+
+public interface UserService {
+	
+	User findById(long id);
+	
+	User findByName(String name);
+	
+	void saveUser(User user);
+	
+	void updateUser(User user);
+	
+	void deleteUserById(long id);
+
+	List<User> findAllUsers(); 
+	
+	void deleteAllUsers();
+	
+	public boolean isUserExist(User user);
+	
+}
